c644c33419cf5e932eacf13e9799869cfc5a36e2
vibhu mahendru <vibhumahendru@gmail.com>
2019-05-10 15:31:12 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // src/App.js
 
 import React, { Component } from 'react';
-// import './App.css'; <-- commented out for styling
+import './App.css';
 // import ConversationsList from './components/ConversationsList';
 import { API_WS_ROOT } from './constants';
 import { API_ROOT } from './constants';
 import { HEADERS } from './constants';
 import { ActionCable } from 'react-actioncable-provider';
+import Notification from './components/Notification';
 
 class App extends Component {
 
@@ -21,7 +22,6 @@ class App extends Component {
   };
 
   componentDidMount = () => {
-    // console.log(API_ROOT);
     fetch(`${API_ROOT}/notifications`)
       .then(res => res.json())
       .then((res)=>{
@@ -59,7 +59,7 @@ class App extends Component {
       headers: HEADERS,
       body: JSON.stringify({
         category: this.state.category,
-        message: "this is a test"
+        message: "This is some sample text."
       })
     });
     this.setState({ message: '' });
@@ -73,7 +73,9 @@ for (const item of this.state.notifications) {
         map.set(item.id, true);    // set any value to Map
         uniqueNotifications.push({
             id: item.id,
-            category: item.category
+            category: item.category,
+            created_at: item.created_at,
+            message:item.message
         });
     }
 }
@@ -89,32 +91,52 @@ for (const item of this.state.notifications) {
 return uniqueNotifications
   }
 
+  handleDate=()=>{
+    let newDate = new Date()
+// let date = newDate.getDate();
+// let month = newDate.getMonth() + 1;
+// let year = newDate.getFullYear();
+// let day = newDate.getDay()
+let date = newDate.toString()
+let dateAr = date.split(" ")
+
+return dateAr[0] + " " + dateAr[1] + " " + dateAr[2]+", " + dateAr[3]
+  }
+
 
   render() {
-    console.log(this.state);
+    console.log(this.handleDate());
     return (
 
-      <div className="App">
-      <ActionCable
-          channel={{ channel: 'NotificationsChannel' }}
-          onReceived={this.handleReceivedConversation}
-        />
-        <input value={this.state.message} onChange={(event)=>this.handleMessageInput(event)} type="text"></input>
-        <select onChange={(event)=>this.handleCategorySelect(event)}>
-          <option value="Assigned Task">Assigned Task</option>
-          <option value="Reminder">Reminder</option>
-          <option selected value="Notification">Notification</option>
-        </select>
-        <button onClick={this.handleSubmit}>submit</button>
-        <br></br>
+      <div className="main-container">
+      <ActionCable channel={{ channel: 'NotificationsChannel' }} onReceived={this.handleReceivedConversation}/>
 
         <div>
-          <h1>Reminders : {this.state.reminders.length}</h1>
-          <h1>Notifications : {this.state.generalNotifications.length}</h1>
-          <h1>Assigned Tasks : {this.state.assignedTasks.length}</h1>
+          <select onChange={(event)=>this.handleCategorySelect(event)}>
+            <option value="Assigned Task">Assigned Task</option>
+            <option value="Reminder">Reminder</option>
+            <option selected value="Notification">Notification</option>
+          </select>
+          <button onClick={this.handleSubmit}>Create New Notification</button>
+          <br></br>
         </div>
+        <div className="dashboard">
+          <div className="date-display">
+          <p id="date-text">{this.handleDate()}</p>
+          </div>
+          <div className="counter-display">
+            <text className="notif-number">{this.state.assignedTasks.length}</text>  <text>Assigned Tasks</text><br></br>
+            <text className="notif-number">{this.state.reminders.length}</text>  <text>Reminders</text><br></br>
+            <text className="notif-number">{this.state.generalNotifications.length}</text>  <text>Notifications</text>
+            <br></br>
+            <br></br>
+            <text className="workspace">My Workspace{`>`}</text>
+          </div>
 
-
+          <div id="notification-info-column">
+            {this.state.notifications.slice(this.state.notifications.length -10, this.state.notifications.length).reverse().map(eachNotification=> <Notification notification={eachNotification} /> )}
+          </div>
+        </div>
       </div>
 
 
---
+++ src/components/Notification.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import '../App.css';
+import Moment from 'react-moment';
+
+class Notification extends Component {
+
+  handleJustNow=()=>{
+    let x = <Moment fromNow>{this.props.notification.created_at}</Moment>
+    return x
+  }
+
+  render() {
+    // console.log(this.handleJustNow());
+    return (
+      <div className="notification-display">
+        <text id="cross">X</text>
+        <div >
+        <strong><Moment fromNow ago>{this.props.notification.created_at}</Moment></strong>
+        </div>
+        <div>
+          Type: {this.props.notification.category}
+        </div>
+        <div>
+          {this.props.notification.message} <text className="workspace">View Task {'>'}</text>
+        </div>
+
+      </div>
+    );
+  }
+
+}
+
+export default Notification;
